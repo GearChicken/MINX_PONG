cf9cd93f1e1c6fc443669f281a8424138f375a62
Benjamin <brpylko@gmail.com>
2013-07-25 00:20:38 -0400

    got almost everything working except scoring. Due to a bug in MINX it segfaults after a while.

--- Pong.cpp
+++ Pong.cpp
@@ -18,12 +18,21 @@
 	*/
 #include "Pong.h"
 #include <iostream>
+#include "Point.h"
+#include "Vector2.h"
+
 
 using namespace MINX_PONG;
 using namespace MINX;
 
-Point paddlePosition = Point(0,0);
-Pong::Pong()
+Point paddlePosition = Point(50,0);
+Point paddle2Position = Point(590,0);
+Point ballPosition = Point(320,240);
+Vector2 ballVelocity = Vector2(1,1);
+int playerScore = 0;
+int computerScore = 0;
+Graphics::Color * white = new Graphics::Color(255,255,255,255);
+Pong::Pong() : Game()
 {
 	//This is the constructor. Put stuff here that should happen when the Game is created.
 	isRunning = true;
@@ -34,8 +43,9 @@ void Pong::Initialize()
 	//Put stuff here that should happen when the Game is initialized.
 	
 	keyboard= new Input::Keyboard(this);
-	keyboard->Initialize();
-
+	paddle2Position.Y = rand() % 380;
+	ballPosition.Y = rand() % 140 + 100;
+	ballPosition.X = rand() % 200 + 220;
 	Game::Initialize();
 
 	SDL_WM_SetCaption("MINX Pong", "MINX Pong");
@@ -64,7 +74,53 @@ void Pong::Update(GameTime * gameTime)
 	{
 		paddlePosition.Y++;
 	}
-	cout << paddlePosition.Y << "\n";
+	if(paddlePosition.Y < 5)
+	{
+		paddlePosition.Y = 5;
+	}
+	if(paddlePosition.Y > 380)
+	{
+		paddlePosition.Y = 380;
+	}
+	if(paddle2Position.Y > 375)
+	{
+		paddle2Position.Y = 375;
+	}
+	if(ballPosition.X < paddlePosition.X + 20 && ballPosition.X > paddlePosition.X && ballPosition.Y > paddlePosition.Y && ballPosition.Y < paddlePosition.Y + 100)
+	{
+		ballVelocity.X*=-1;
+	}
+	if(ballPosition.X > paddle2Position.X && ballPosition.X < paddle2Position.X + 20 && ballPosition.Y > paddle2Position.Y && ballPosition.Y < paddle2Position.Y + 100)
+	{
+		ballVelocity.X*=-1;
+	}
+	if(ballPosition.Y < 0 || ballPosition.Y > 464)
+	{
+		ballVelocity.Y*=-1;
+	}
+	if(ballVelocity.X > 0)
+	{
+		if(ballPosition.Y < paddle2Position.Y+50)
+		{
+			paddle2Position.Y--;
+		} else
+		if(ballPosition.Y > paddle2Position.Y+50)
+		{
+			paddle2Position.Y++;
+		}
+	}
+	if(ballPosition.X > 624){
+		computerScore++;
+		ballPosition.Y = rand() % 140 + 100;
+		ballPosition.X = rand() % 200 + 220;
+	}
+	if(ballPosition.X < 0){
+		playerScore++;
+		ballPosition.Y = rand() % 140 + 100;
+		ballPosition.X = rand() % 200 + 220;
+	}
+	ballPosition.X += (int)ballVelocity.X;
+	ballPosition.Y += (int)ballVelocity.Y;
 	SDL_Delay(5);
 	Game::Update(gameTime);
 }
@@ -74,7 +130,9 @@ void Pong::Draw(GameTime * gameTime)
 
 	SDL_FillRect(gameWindow->screen, NULL, 0x000000);
 
-	Graphics::Primitives::drawRectangle(new Graphics::Color(255,255,255,0), 50, paddlePosition.Y, 20, 100, gameWindow->screen);
+	Graphics::Primitives::drawRectangle(white, paddlePosition.X, paddlePosition.Y, 20, 100, gameWindow->screen);
+	Graphics::Primitives::drawRectangle(white, ballPosition.X, ballPosition.Y, 16, 16, gameWindow->screen);
+	Graphics::Primitives::drawRectangle(white,paddle2Position.X, paddle2Position.Y, 20, 100, gameWindow->screen);
 	
 	//Put stuff here to draw your game each frame.
 	Game::Draw(gameTime);
