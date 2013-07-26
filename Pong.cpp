d5c98741121ddc6e9014a40e9d49245708861af1
Liam Middlebrook <liammiddlebrook@gmail.com>
2013-07-25 21:23:30 -0400

    Fixed ball speed issue Added binary output of score

--- Pong.cpp
+++ Pong.cpp
@@ -16,15 +16,18 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 	*/
+#include "Graphics/GameWindow.h"
 #include "Pong.h"
 #include <iostream>
 #include "Point.h"
 #include "Vector2.h"
-
+#include <string>
+#include <algorithm>
 
 using namespace MINX_PONG;
 using namespace MINX;
-
+using namespace MINX::Graphics;
+using namespace std;
 Point paddlePosition = Point(50,0);
 Vector2 paddleVelocity = Vector2(0,0);
 Point paddle2Position = Point(590,0);
@@ -34,6 +37,34 @@ Vector2 ballVelocity = Vector2(1,1);
 int playerScore = 0;
 int computerScore = 0;
 Graphics::Color * white = new Graphics::Color(255,255,255,255);
+string intToBinary(int number)
+{
+	string result;
+	do result.push_back('0' + (number & 1));
+	while (number >>=1);
+	
+	reverse(result.begin(), result.end());
+	return result;
+}
+void drawScore(string scoreString, Point drawingPoint, GameWindow* gameWindow)
+{
+	for(string::size_type i = 0; i < scoreString.size(); ++i)
+	{
+		if(scoreString[i] == *"0")
+		{
+			Graphics::Primitives::drawOutlineRectangle(white, drawingPoint.X + i*10, drawingPoint.Y, 5, 10, gameWindow->screen);
+		}
+		if(scoreString[i] == *"1")
+		{
+			Graphics::Primitives::drawRectangle(white, drawingPoint.X + i*10, drawingPoint.Y, 5, 10, gameWindow->screen);
+		}
+	}
+}
+void drawScores(GameWindow* gameWindow)
+{
+	drawScore(intToBinary(playerScore), Point(50,50), gameWindow);
+	drawScore(intToBinary(computerScore), Point(590,430), gameWindow);
+}
 Pong::Pong() : Game()
 {
 	//This is the constructor. Put stuff here that should happen when the Game is created.
@@ -91,11 +122,11 @@ void Pong::Update(GameTime * gameTime)
 	if(ballPosition.X < paddlePosition.X + 20 && ballPosition.X > paddlePosition.X && ballPosition.Y + 16 > paddlePosition.Y && ballPosition.Y < paddlePosition.Y + 100)
 	{
 		ballVelocity.X*=-1;
-		ballVelocity.Y += paddleVelocity.Y;
+		ballVelocity.Y += paddleVelocity.Y/3;
 	}
 	if(ballPosition.X + 16 > paddle2Position.X && ballPosition.X + 16 < paddle2Position.X + 20 && ballPosition.Y + 16 > paddle2Position.Y && ballPosition.Y < paddle2Position.Y + 100)
 	{
-		ballVelocity.Y += paddle2Velocity.Y;
+		ballVelocity.Y += paddle2Velocity.Y/3;
 		ballVelocity.X*=-1;
 	}
 	if(ballPosition.Y < 0 || ballPosition.Y > 464)
@@ -114,16 +145,21 @@ void Pong::Update(GameTime * gameTime)
 		}
 	}
 	if(ballPosition.X > 624){
-		computerScore++;
+		playerScore++;
 		ballPosition.Y = rand() % 140 + 100;
 		ballPosition.X = rand() % 200 + 220;
 		ballVelocity = Vector2(1,1);
 	}
 	if(ballPosition.X < 0){
-		playerScore++;
+		computerScore++;
 		ballPosition.Y = rand() % 140 + 100;
 		ballPosition.X = rand() % 200 + 220;
 		ballVelocity = Vector2(1,1);	
+	}
+	if(ballVelocity.lengthSquared() > 9)
+	{
+		ballVelocity = ballVelocity.normalize();
+		ballVelocity = ballVelocity * 3;
 	}
 	paddlePosition.Y += paddleVelocity.Y;
 	paddle2Position.Y += paddle2Velocity.Y;
@@ -143,7 +179,7 @@ void Pong::Draw(GameTime * gameTime)
 	Graphics::Primitives::drawRectangle(white, paddlePosition.X, paddlePosition.Y, 20, 100, gameWindow->screen);
 	Graphics::Primitives::drawRectangle(white, ballPosition.X, ballPosition.Y, 16, 16, gameWindow->screen);
 	Graphics::Primitives::drawRectangle(white,paddle2Position.X, paddle2Position.Y, 20, 100, gameWindow->screen);
-	
+	drawScores(gameWindow);
 	//Put stuff here to draw your game each frame.
 	Game::Draw(gameTime);
 }
