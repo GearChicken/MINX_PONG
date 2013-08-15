848d4142db8e7b1003b06df9fd632c25b1101cce
Liam Middlebrook <liammiddlebrook@gmail.com>
2013-08-15 16:22:01 -0400

    Got MINX_PONG working in windows!

--- make.sh
+++ make.sh
@@ -1,1 +1,1 @@
-g++ -Wall -std=c++11 *.cpp -I/home/liam/c++/MINX/src -I/home/benjamin/MINX/src -lMINX -lSDL -o pong
+g++ -Wall -std=c++11 ./src/*.cpp -I/home/liam/c++/MINX/src -I/home/benjamin/MINX/src -lMINX -lSDL -o pong
